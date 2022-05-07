df37f647bf6a9fb56e7ed6b6544fab3544bbf007
Mohit Sharma <starmohit14@gmail.com>
2022-05-07 07:55:57 +0530

    Data Type

---
+++ Module - 2/data-types.js
@@ -0,0 +1,29 @@
+//number
+var x = 10;
+console.log(typeof x);
+console.log(x);
+
+//string
+var y = "Mohit Sharma"
+console.log(typeof y);
+console.log(y);
+
+//Array
+var cars = ["Mohit", "Abhishek", "Om"];
+console.log(typeof cars);
+console.log(cars);
+
+//Object
+var obj = {Name : 'Mohit', Place : 'Danpur'};
+console.log(typeof obj);
+console.log(obj);
+
+//Undefined
+var z;
+console.log(typeof z);
+console.log(z);
+
+//Null
+var n = null;
+console.log(typeof n);
+console.log(n);
