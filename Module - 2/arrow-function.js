aea56b99b52e9f8dfac710e543c24609b012a69c
Mohit Sharma <starmohit14@gmail.com>
2022-05-08 09:00:43 +0530

    Functions

---
+++ Module - 2/arrow-function.js
@@ -0,0 +1,4 @@
+
+let functionPrint = (num1, num2) =>{console.log(num1+num2);}
+
+functionPrint(2, 2);
---
+++ Module - 2/functionAsVariables.js
@@ -0,0 +1,7 @@
+
+const functionPrint = function (num1, num2)
+{
+    console.log(num1+num2);
+}
+console.log(typeof functionPrint);
+functionPrint(1,1);
