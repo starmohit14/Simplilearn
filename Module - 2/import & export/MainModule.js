58843de1ca0509c8788e94ae5267ff177d02ede5
Mohit Sharma <starmohit14@gmail.com>
2022-05-19 06:13:27 +0530

    export & import

---
+++ Module - 2/import & export/MainModule.js
@@ -0,0 +1,6 @@
+const arithModule = require("./arithmaticModule");
+
+console.log("Add : "+arithModule.add(1, 1));
+console.log("Sub : "+arithModule.sub(2, 1));
+console.log("Mul : "+arithModule.mul(2, 1));
+console.log("Div : "+arithModule.div(2, 2));
---
+++ Module - 2/import & export/arithmaticModule.js
@@ -0,0 +1,20 @@
+const obj = {
+    add : function (a, b)
+    {
+        return a+b;
+    },
+    sub : function (a, b)
+    {
+        return a-b;
+    },
+    mul : function (a, b)
+    {
+        return a*b;
+    },
+    div : function (a, b)
+    {
+        return a/b;
+    }
+}
+
+module.exports = obj;
